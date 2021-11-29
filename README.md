708c52e5665f94d34bc70fa99d61b1e9c2388f4e
kron <kron.isant@gmail.com>
2021-11-29 19:15:59 +0300

    Change cli and add postgres download feature

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-# СКРИПТ ДЛЯ СКАЧИВАНИЯ ДИСТРИБУТИВОВ ПЛАТФОРМЫ С ПОРТАЛА releases.1c.ru
+# СКРИПТ ДЛЯ СКАЧИВАНИЯ ДИСТРИБУТИВОВ С ПОРТАЛА releases.1c.ru
 
-Для PowerShell:
+### Скачать 1С 8.3.19.1417 в каталог distr\1c_win64 под Windows 64 бита
+Для **PowerShell**:
 ```powershell
-$PLATFORM_VER="8.3.19.1417";.\download1c.ps1 distr\win64
+.\download1c.ps1 distr\1c_win64 -v 8.3.19.1417 -t windows64full -e rar
 ```
 
-Для bash:
+
+### Скачать PostgreSQL 12.7-5.1C в каталог distr\postrges_win64 под Windows 64 бита
+Для **PowerShell**:
+```powershell
+.\download1c.ps1 distr\postrges_win64 -k postgres -v 12.7-5.1C -t x64 -e zip
+```
+
+### Скачать 1С 8.3.19.1417 в каталог distr\1c_deb64 под Linux(deb) 64 бита
+Для **bash**:
 ```bash
-PLATFORM_VER="8.3.19.1417" PLATFORM_TYPE="deb64" ./download1c.sh distr/deb64
+./download1c.sh distr/1c_deb64 -v 8.3.19.1417 -t deb64 -e tar.gz
+```
+
+### Скачать PostgreSQL 12.7-5.1C в каталог distr\postrges_deb64 под Linux(deb) 64 бита
+Для **bash**:
+```bash
+./download1c.sh distr/postrges_deb64 -k postgres -v 12.7-5.1C -t amd64_deb -e tar.bz2
 ```
 
 Для запуска необходимо задать параметры:
-* Переменные **PORTAL_USER** и **PORTAL_PASS** можно указать в файле скрипта, расскоментировав соответствующие строки,
- или задать через переменные окружения, или в отдельном файле **credentials**
-* Переменные **PLATFORM_VER** и **PLATFORM_TYPE** и **DISTR_EXTENSION** можно указать в переменных окружения или в файле
-* Путь к каталогу, куда будет загружен дистрибутив, указывается в первом параметре запуска скрипта. Если параметр не указан,
-то дистрибутив будет загружен в подкаталог **distr** места запуска скрипта
+ * <первый параметр без ключа> | DISTR_PATH - путь к каталогу, куда скачивается дистрибутив. По умолчанию: '.\distr' (подкаталог относительно текущего каталога);
+ * -u | PORTAL_USER - пользователь портала 1С;
+ * -p | PORTAL_PASS - пароль пользователя портала 1С;
+ * -k | DISTR_KIND - вид дистриьутива: "platform", "postgres";
+ * -v | DISTR_VER - версия дистрибутива (например, **8.3.19.1417** для 1С или **12.7-5.1C** для Postgres);
+ * -t | DISTR_TYPE - тип платформы (например, **windows64full** или **deb64**);
+ * -e | DISTR_EXTENSION - расширение файла дистрибутива (например, **rar**).
+
+Переменные **PORTAL_USER** и **PORTAL_PASS** можно указать в файле скрипта, или задать через переменные окружения, или в отдельном файле **credentials**
--- download1c.sh
+++ download1c.sh
@@ -6,17 +6,46 @@
 #PORTAL_PASS=YOUR_PORTAL_PASSWORD
 [ -r credentials ] && source credentials
 
+# Если первым параметром указан не ключ, считаем, что это путь к каталогу
+if [ ! ${1:0:1} = '-' ] 
+then 
+    DISTR_PATH=$1
+    shift
+else
+    # По умолчанию дистрибутивы скачиваются в подкаталог distr текущего каталога. 
+    # Но можно указать путь к каталогу дистрибутивом в первом параметре
+    DISTR_PATH=./distr
+fi
+
+while getopts 'u:p:k:v:t:e:' opt
+do
+    case $opt in 
+        u) PORTAL_USER=$OPTARG;;
+        p) PORTAL_PASS=$OPTARG;;
+        k) DISTR_KIND=$OPTARG;;
+        v) DISTR_VER=$OPTARG;;
+        t) DISTR_TYPE=$OPTARG;;
+        e) DISTR_EXTENSION=$OPTARG;;
+    esac
+done
+
+
 # Параметры, определяющие скачиваемый реализ
-[ -z $PLATFORM_VER ] && PLATFORM_VER=8.3.20.1613
-[ -z $PLATFORM_TYPE ] && PLATFORM_TYPE=server64
+[ -z $DISTR_KIND ] && DISTR_KIND=platform # "platform", "postgres"
+[ -z $DISTR_VER ] && DISTR_VER=8.3.19.1417
+[ -z $DISTR_TYPE ] && DISTR_TYPE=deb64
 [ -z $DISTR_EXTENSION ] && DISTR_EXTENSION=tar.gz
-# По умолчанию дистрибутивы скачиваются в подкаталог distr текущего каталога. 
-# Но можно указать путь к каталогу дистрибутивом в первом параметре
-DISTR_PATH=./distr
-[ $# = 1 ] && DISTR_PATH=$1
 
 # Страница с сылками на релиз
-release_download_page_url="https://releases.1c.ru/version_file?nick=Platform83&ver=${PLATFORM_VER}&path=Platform%5c${PLATFORM_VER//\./_}%5c${PLATFORM_TYPE}_${PLATFORM_VER//\./_}.${DISTR_EXTENSION}"
+case $DISTR_KIND in 
+    platform) 
+        release_download_page_url="https://releases.1c.ru/version_file?nick=Platform83&ver=${DISTR_VER}&path=Platform%5c${DISTR_VER//\./_}%5c${DISTR_TYPE}_${DISTR_VER//\./_}.${DISTR_EXTENSION}";;
+    postgres) 
+        release_download_page_url="https://releases.1c.ru/version_file?nick=AddCompPostgre&ver=${DISTR_VER}&path=AddCompPostgre%5c${DISTR_VER//[\.-]/_}%5cpostgresql_${DISTR_VER//-/_}_${DISTR_TYPE}.${DISTR_EXTENSION}";;
+    *) 
+        echo "Некорректный тип дистрибутива: $DISTR_KIND"
+        exit 1;;
+esac
 echo "Страница дистрибутива: $release_download_page_url"
 
 # Получение cookies для портала 1С. Cookies сохраняем во временный файл .portal_1c_cookies.tmp
@@ -24,19 +53,19 @@ execution_code=`wget -qO- https://releases.1c.ru | grep -oP '(?<=input type="hid
 post_data="username=${PORTAL_USER}&password=${PORTAL_PASS}&execution=${execution_code}&_eventId=submit"
 wget -qO- --keep-session-cookies --save-cookies .portal_1c_cookies.tmp --post-data $post_data https://login.1c.ru/login > /dev/null
 
-# На странице с ссылками на релиз, находим сгенерирвонный URL, ведущий к дистрибутиву
+# На странице с ссылками на релиз, находим сгенерированный URL, ведущий к дистрибутиву
 release_download_page_data=`wget -qO- --load-cookies .portal_1c_cookies.tmp $release_download_page_url`
 release_url=`echo $release_download_page_data | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив<)'`
 if [ "x$release_url" != "x" ]
 then
     # Скачиваем дистрибутив
     mkdir -p "$DISTR_PATH"
-    distr_fullname="$DISTR_PATH/$PLATFORM_TYPE_$PLATFORM_VER.$DISTR_EXTENSION"
+    distr_fullname="$DISTR_PATH/$DISTR_TYPE_$DISTR_VER.$DISTR_EXTENSION"
     echo "Дистрибутив скачивается ($distr_fullname) Подождите..."
     wget -O $distr_fullname --load-cookies .portal_1c_cookies.tmp $release_url
 else
     echo "Не удалось получить url-для скачивания"
-    [[ `echo $release_download_page_data` =~ "Указанный файл не найден" ]] && echo "Указанный релиз $PLATFORM_VER с расширением $DISTR_EXTENSION не существует для платформы $PLATFORM_TYPE!"
+    [[ `echo $release_download_page_data` =~ "Указанный файл не найден" ]] && echo "Указанный релиз $DISTR_VER с расширением $DISTR_EXTENSION не существует для платформы $DISTR_TYPE!"
 fi
 echo "Загрузка завершена ($(ls -lh "$distr_fullname" 2> /dev/null | cut -d' ' -f5 2> /dev/null))!"
 # Удаляем файл с cookies
